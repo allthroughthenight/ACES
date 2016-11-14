fccc202098cbcdabeff52a6585efcdf4d6f25a3a
Hendricks <dubmore@gmail.com>
2016-11-14 09:35:01 -1000

    web2py

---
+++ web2py/new/windows/applications/welcome/controllers/test.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import subprocess
+
+def listing():
+    functionCall = "Listing call"
+    return functionCall
+
+def hello():
+    directory = listing()
+    return listing()
