04a64eb7a274a63359d8a0b4af19717ee4d224e7
Hendricks <dubmore@gmail.com>
2016-12-27 12:49:20 -1000

    removed old web2py

--- web2py/new/windows/parameters_8000.py
+++
@@ -1,1 +0,0 @@
-password="pbkdf2(1000,20,sha512)$bdc33393baf9ca58$a567ce2a7c4eaf695bb778b97b1fa7add0f3c13b"
--- web2py/old/test/ERRSTP.py
+++
@@ -1,9 +0,0 @@
-from numpy import pi, tanh
-
-def ERRSTP(H,d,L):
-    steep=H/L;
-    k=(2*pi)/L;
-    maxstp=0.142*tanh(k*d);
-    return steep,maxstp
-
-
