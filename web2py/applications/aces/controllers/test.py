e45e86ce0611326a2201d7338111af48099ce61c
Hendricks <dubmore@gmail.com>
2016-12-27 18:55:14 -1000

    made menu with all functions listed in drop downs

---
+++ web2py/applications/aces/controllers/test.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# try something like
+# def index(): return dict(message="hello from test.py")
+def inlet_process():
+    return 'hello world'
+def hello2():
+    return {'a':10, 'b':'hello world'}
