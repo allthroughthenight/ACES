7b50ba4bdabc870e2b39cbdd566063822c414d07
Hendricks <dubmore@gmail.com>
2017-01-04 16:41:30 -1000

    renamed aces to fortran

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8ac32483adb221cb$98a54cf8c84ebadb2dddbf80dbd3e59bcde4dfa4"
+password="pbkdf2(1000,20,sha512)$b1d2dfdfdd5f19b0$e4cc9cea9bf5a5d57cc53e1dac8869342beca1f6"
