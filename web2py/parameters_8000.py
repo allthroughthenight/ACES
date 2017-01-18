dc9419b0f531ecb798f85b04d259b93bfc59458a
Hendricks <dubmore@gmail.com>
2017-01-17 19:47:11 -1000

    updated to-do list

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b1d2dfdfdd5f19b0$e4cc9cea9bf5a5d57cc53e1dac8869342beca1f6"
+password="pbkdf2(1000,20,sha512)$bce203d904f5e302$e74f27077ff56d8a744f1e47a14ef5e3b24cad6d"
