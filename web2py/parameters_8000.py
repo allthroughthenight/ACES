d177288de6af73b6edcbc6040227a3c4d60f0338
Hendricks <dubmore@gmail.com>
2017-04-03 14:51:31 -1000

    broke linear wave theory

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8cd9f6a4ed505dea$76f5b8b6bd381031312c6579d98a2c6d74ac8b10"
+password="pbkdf2(1000,20,sha512)$87854a3d85bb71cc$f7ec645cf0a99d50a590b1f4d6e371ba2930d9fb"
