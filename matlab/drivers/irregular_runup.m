492a1f2bc8f1446422433d26f1c459cab2248676
minixix <onat.yaprak@gmail.com>
2017-06-10 12:25:23 -1000

    multicase added

--- matlab/drivers/irregular_runup.m
+++ matlab/drivers/irregular_runup.m
@@ -35,22 +35,26 @@ SET_PATHS();
 
 [single_case] = USER_INPUT_SINGLE_MULTI_CASE();
 
-[metric, g, rho, labelUnitDist, labelUnitWt] = USER_INPUT_METRIC_IMPERIAL();
+[metric, g, labelUnitDist, labelUnitWt] = USER_INPUT_METRIC_IMPERIAL();
 
 if single_case
-    if metric
-        [Hs0] = USER_INPUT_DATA_VALUE('Enter Hs0: deepwater significant wave height (m): ', 0.1, 100.0);
-    else
-        [Hs0] = USER_INPUT_DATA_VALUE('Enter Hs0: deepwater significant wave height (ft): ', 0.1, 100.0);
-    end
+    [Hs0] = USER_INPUT_DATA_VALUE(['Enter Hs0: deepwater significant wave height (' labelUnitDist '): '], 0.1, 100.0);
     
     [Tp] = USER_INPUT_DATA_VALUE('Enter Tp: peak energy wave period (sec): ', 0.1, 100.0);
     
     [cottheta] = USER_INPUT_DATA_VALUE('Enter cottheta: cotangent of foreshore slope: ', 0.1, 100.0);
+    
+    numCases = 1;
 else
-    Hs0=4.6;
-    Tp=9.50;
-    cottheta=13.0;
+    multiCaseData = {...
+        ['Hs0: deepwater significant wave height (' labelUnitDist ')'], 0.1, 100.0;...
+        'Tp: peak energy wave period (sec)', 0.1, 100.0;...
+        'cottheta: cotangent of foreshore slope', 0.1, 100.0};
+    [varData, numCases] = USER_INPUT_MULTI_MODE(multiCaseData);
+    
+    Hs0List = varData(1, :);
+    TpList = varData(2, :);
+    cotthetaList = varData(3, :);
 end
 
 %Coefficients provided by Mase (1989)
@@ -65,21 +69,29 @@ b13=0.70;
 aavg=0.88;
 bavg=0.69;
 
-L0=g*(Tp^2)/(2*pi);
-steep=Hs0/L0;
-assert(steep<0.142,'Error: Input wave unstable (Max: 0.142, [H/L] = %0.4f)',steep')
+for loopIndex = 1:numCases
+    if ~single_case
+        Hs0 = Hs0List(loopIndex);
+        Tp = TpList(loopIndex);
+        cottheta = cotthetaList(loopIndex);
+    end
+    
+    L0=g*(Tp^2)/(2*pi);
+    steep=Hs0/L0;
+    assert(steep<0.142,'Error: Input wave unstable (Max: 0.142, [H/L] = %0.4f)',steep')
 
-tantheta=1/cottheta;
-I=tantheta/sqrt(Hs0/L0);
+    tantheta=1/cottheta;
+    I=tantheta/sqrt(Hs0/L0);
 
-Rmax=Hs0*amax*(I^bmax);
-R2=Hs0*a2*(I^b2);
-R110=Hs0*a110*(I^b110);
-R13=Hs0*a13*(I^b13);
-Ravg=Hs0*aavg*(I^bavg);
+    Rmax=Hs0*amax*(I^bmax);
+    R2=Hs0*a2*(I^b2);
+    R110=Hs0*a110*(I^b110);
+    R13=Hs0*a13*(I^b13);
+    Ravg=Hs0*aavg*(I^bavg);
 
-fprintf('%s \t\t\t\t\t %-6.2f %s \n','Maximum runup',Rmax,labelUnitDist)
-fprintf('%s \t %-6.2f %s \n','Runup exceeded by 2% of runup',R2,labelUnitDist)
-fprintf('%s \t %-6.2f %s \n','Avg. of highest 1/10 runups',R110,labelUnitDist)
-fprintf('%s \t\t %-6.2f %s \n','Avg. of highest 1/3 runups',R13,labelUnitDist)
-fprintf('%s \t\t\t\t\t %-6.2f %s \n','Maximum runup',Ravg,labelUnitDist)
+    fprintf('%s \t\t\t\t\t %-6.2f %s \n','Maximum runup',Rmax,labelUnitDist)
+    fprintf('%s \t %-6.2f %s \n','Runup exceeded by 2% of runup',R2,labelUnitDist)
+    fprintf('%s \t %-6.2f %s \n','Avg. of highest 1/10 runups',R110,labelUnitDist)
+    fprintf('%s \t\t %-6.2f %s \n','Avg. of highest 1/3 runups',R13,labelUnitDist)
+    fprintf('%s \t\t\t\t\t %-6.2f %s \n','Maximum runup',Ravg,labelUnitDist)
+end
