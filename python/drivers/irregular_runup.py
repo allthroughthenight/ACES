0f8b3567748f33df104b2e5d4939df1155b85555
minixix <onat.yaprak@gmail.com>
2017-07-04 12:55:58 -1000

    Added irregular_runup and toe_design

--- python/drivers/irregular_runup.py
+++ python/drivers/irregular_runup.py
@@ -1,6 +1,12 @@
+import sys
 import math
+sys.path.append('../functions')
 
-## ACES Update to MATLAB
+from base_driver import BaseDriver
+from helper_objects import BaseField
+import USER_INPUT
+
+## ACES Update to python
 #-------------------------------------------------------------
 # Driver for Irregular Wave Runup on Beaches (page 5-1 in ACES # User's Guide).
 # Provides an approach to calculate runup statistical parameters for wave
@@ -30,41 +36,121 @@ import math
 #   I: Irribarren number
 #-------------------------------------------------------------
 
-def irregular_runup(Hs0, Tp, cottheta):
-    Hs0 = 4.6
-    Tp = 9.50
-    cottheta = 13.0
-    g = 32.17
+class IrregularRunup(BaseDriver):
+    def __init__(self, Hs0 = None, Tp = None, cottheta = None):
+        if Hs0 != None:
+            self.isSingleCase = True
+            self.defaultValueHs0 = Hs0
+        if Tp != None:
+            self.isSingleCase = True
+            self.defaultValueTp = Tp
+        if cottheta != None:
+            self.isSingleCase = True
+            self.defaultValue_cottheta = cottheta
 
-    #Coefficients provided by Mase (1989)
-    amax = 2.32
-    bmax = 0.77
-    a2 = 1.86
-    b2 = 0.71
-    a110 = 1.70
-    b110 = 0.71
-    a13 = 1.38
-    b13 = 0.70
-    aavg = 0.88
-    bavg = 0.69
+        super(IrregularRunup, self).__init__()
+    # end __init__
 
-    L0 = g * (Tp**2) / (2 * math.pi)
-    steep = Hs0 / L0
-    assert (steep < 0.142), ('Error: Input wave unstable (Max: 0.142, [H/L] = %0.4f' % steep)
+    def defineInputDataList(self):
+        self.inputList = []
 
-    tantheta = 1 / cottheta
-    I = tantheta / math.sqrt(Hs0 / L0)
+        if not hasattr(self, "defaultValueHs0"):
+            self.inputList.append(BaseField(\
+                "Hs0: deepwater significant wave height (%s)" %\
+                (self.labelUnitDist), 0.1, 100.0))
+        if not hasattr(self, "defaultValueTp"):
+            self.inputList.append(BaseField(\
+                "Tp: peak energy wave period (sec)", 0.1, 100.0))
+        if not hasattr(self, "defaultValue_cottheta"):
+            self.inputList.append(BaseField(\
+                "cottheta: cotangent of foreshore slope", 0.1, 100.0))
+    # end defineInputDataList
 
-    Rmax = Hs0 * amax * (I**bmax)
-    R2 = Hs0 * a2 * (I**b2)
-    R110 = Hs0 * a110 * (I**b110)
-    R13 = Hs0 * a13 * (I**b13)
-    Ravg = Hs0 * aavg * (I**bavg)
+    def fileOutputRequestInit(self):
+        self.fileOutputRequestMain(defaultFilename = "irregular_runup")
 
-    print('%s \t %-6.2f \n' % ('Maximum runup', Rmax))
-    print('%s \t %-6.2f \n' % ('Runup exceeded by 2% of runup', R2))
-    print('%s \t %-6.2f \n' % ('Avg. of highest 1 / 10 runups', R110))
-    print('%s \t %-6.2f \n' % ('Avg. of highest 1 / 3 runups', R13))
-    print('%s \t %-6.2f \n' % ('Maximum runup', Ravg))
+    def getCalcValues(self, caseInputList):
+        currIndex = 0
 
-irregular_runup(0, 0, 0)
+        if hasattr(self, "defaultValueHs0"):
+            Hs0 = self.defaultValueHs0
+        else:
+            Hs0 = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValueTp"):
+            Tp = self.defaultValueTp
+        else:
+            Tp = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_cottheta"):
+            cottheta = self.defaultValue_cottheta
+        else:
+            cottheta = caseInputList[currIndex]
+
+        return Hs0, Tp, cottheta
+    # end getCalcValues
+
+    def performCalculations(self, caseInputList, caseIndex = 0):
+        Hs0, Tp, cottheta = self.getCalcValues(caseInputList)
+
+        #Coefficients provided by Mase (1989)
+        amax = 2.32
+        bmax = 0.77
+        a2 = 1.86
+        b2 = 0.71
+        a110 = 1.70
+        b110 = 0.71
+        a13 = 1.38
+        b13 = 0.70
+        aavg = 0.88
+        bavg = 0.69
+
+        L0 = self.g * (Tp**2) / (2 * math.pi)
+        steep = Hs0 / L0
+        if not (steep < 0.142):
+            print('Error: Input wave unstable (Max: 0.142, [H/L] = %0.4f' % steep)
+            return
+
+        tantheta = 1 / cottheta
+        I = tantheta / math.sqrt(Hs0 / L0)
+
+        Rmax = Hs0 * amax * (I**bmax)
+        R2 = Hs0 * a2 * (I**b2)
+        R110 = Hs0 * a110 * (I**b110)
+        R13 = Hs0 * a13 * (I**b13)
+        Ravg = Hs0 * aavg * (I**bavg)
+
+        print('%s\t\t\t%6.2f %s' % ('Maximum runup', Rmax, self.labelUnitDist))
+        print('%s\t%6.2f %s' % ('Runup exceeded by 2% of runup', R2, self.labelUnitDist))
+        print('%s\t%6.2f %s' % ('Avg. of highest 1 / 10 runups', R110, self.labelUnitDist))
+        print('%s\t%6.2f %s' % ('Avg. of highest 1 / 3 runups', R13, self.labelUnitDist))
+        print('%s\t\t\t%6.2f %s' % ('Average runup', Ravg, self.labelUnitDist))
+
+        dataDict = {"Hs0": Hs0, "Tp": Tp, "cottheta": cottheta,\
+            "Rmax": Rmax, "R2": R2, "R110": R110, "R13": R13, "Ravg": Ravg}
+        self.fileOutputWriteMain(dataDict, caseIndex)
+    # end performCalculations
+
+    def fileOutputWriteData(self, dataDict):
+        self.fileRef.write("Input\n")
+        self.fileRef.write("Hs0\t\t\t%6.2f %s\n" %\
+            (dataDict["Hs0"], self.labelUnitDist))
+        self.fileRef.write("Tp\t\t\t%6.2f s\n" % dataDict["Tp"])
+        self.fileRef.write("cottheta\t\t%6.2f\n\n" % dataDict["cottheta"])
+
+        self.fileRef.write('%s\t\t\t%6.2f %s\n' %\
+            ('Maximum runup', dataDict["Rmax"], self.labelUnitDist))
+        self.fileRef.write('%s\t%6.2f %s\n' %\
+            ('Runup exceeded by 2% of runup', dataDict["R2"], self.labelUnitDist))
+        self.fileRef.write('%s\t%6.2f %s\n' %\
+            ('Avg. of highest 1 / 10 runups', dataDict["R110"], self.labelUnitDist))
+        self.fileRef.write('%s\t%6.2f %s\n' %\
+            ('Avg. of highest 1 / 3 runups', dataDict["R13"], self.labelUnitDist))
+        self.fileRef.write('%s\t\t\t%6.2f %s\n' %\
+            ('Average runup', dataDict["Ravg"], self.labelUnitDist))
+    # end fileOutputWriteData
+
+
+driver = IrregularRunup()
--- python/drivers/toe_design.py
+++ python/drivers/toe_design.py
@@ -1,11 +1,15 @@
 import math
 import sys
 sys.path.append('../functions')
+
+from base_driver import BaseDriver
+from helper_objects import BaseField
+import USER_INPUT
 from ERRSTP import ERRSTP
 from ERRWAVBRK2 import ERRWAVBRK2
 from WAVELEN import WAVELEN
 
-## ACES Update to MATLAB
+## ACES Update to python
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 # Driver for Toe Protection Design (page 4 - 2 in ACES User's Guide).
 # Determines armor stone size and width of a toe protection apron for
@@ -41,60 +45,205 @@ from WAVELEN import WAVELEN
 #   H20weight: specific weight of water
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
-def toe_design(H, T, ds, cotphi, Kp, de, ht, unitwt):
-    H = 5
-    T = 12
-    ds = 20
-    cotphi = 100.0
-    Kp = 1.5
-    de = 10
-    ht = 4.50
-    unitwt = 165
+class ToeDesign(BaseDriver):
+    def __init__(self, H = None, T = None, ds = None, cotphi = None,\
+        Kp = None, de = None, ht = None, unitwt = None):
+        if H != None:
+            self.isSingleCase = True
+            self.defaultValueH = H
+        if T != None:
+            self.isSingleCase = True
+            self.defaultValueT = T
+        if ds != None:
+            self.isSingleCase = True
+            self.defaultValue_ds = ds
+        if cotphi != None:
+            self.isSingleCase = True
+            self.defaultValue_cotphi = cotphi
+        if Kp != None:
+            self.isSingleCase = True
+            self.defaultValueKp = Kp
+        if de != None:
+            self.isSingleCase = True
+            self.defaultValue_de = de
+        if ht != None:
+            self.isSingleCase = True
+            self.defaultValue_ht = ht
+        if unitwt != None:
+            self.isSingleCase = True
+            self.defaultValue_unitwt = unitwt
 
-    rho = 1.989
-    g = 32.17
-    H20weight = g  *  rho
+        super(ToeDesign, self).__init__()
+    # end __init__
 
-    specgrav = unitwt  /  H20weight
+    def userInput(self):
+        super(ToeDesign, self).userInput()
 
-    dl = ds - ht
-    m = 1 / cotphi
+        self.water, self.rho = USER_INPUT.SALT_FRESH_WATER(self.isMetric)
+    # end userInput
 
-    # doesn't pass with values given in user manual
-    if ((ds / T**2) <= 0.0037424):
-        print('Error: Limiting value detected...Hbs cannot be solved.')
-        return
+    def defineInputDataList(self):
+        self.inputList = []
 
-    Hbs = ERRWAVBRK2(T, m, ds)
-    if H >= Hbs:
-        print('Error: Wave broken at structure (Hbs  =  #6.2f m)' % (Hbs))
-        return
+        if not hasattr(self, "defaultValueH"):
+            self.inputList.append(BaseField(\
+                "Hi: wave height (%s)" % (self.labelUnitDist), 0.1, 100.0))
+        if not hasattr(self, "defaultValueT"):
+            self.inputList.append(BaseField(\
+                "T: wave period (sec)", 1.0, 1000.0))
+        if not hasattr(self, "defaultValue_ds"):
+            self.inputList.append(BaseField(\
+                "ds: water depth at structure (%s)" % (self.labelUnitDist), 0.1, 200.0))
+        if not hasattr(self, "defaultValue_cotphi"):
+            self.inputList.append(BaseField(\
+                "cotphi: cotangent of nearshore slope", 5.0, 10000.0))
+        if not hasattr(self, "defaultValueKp"):
+            self.inputList.append(BaseField(\
+                "Kp: passive earth pressure coefficient", 0.0, 50.0))
+        if not hasattr(self, "defaultValue_de"):
+            self.inputList.append(BaseField(\
+                "de: sheet-pile penetration depth (%s)" % (self.labelUnitDist), 0.0, 200.0))
+        if not hasattr(self, "defaultValue_ht"):
+            self.inputList.append(BaseField(\
+                "ht: height of toe protection layer above mudline (%s)" % (self.labelUnitDist), 0.1, 200.0))
+        if not hasattr(self, "defaultValue_unitwt"):
+            self.inputList.append(BaseField(\
+                "unitwt: unit weight of rock (%s/%s^3)" % (self.labelUnitWt, self.labelUnitDist), 1.0, 99999.0))
+    # end defineInputDataList
 
-    L, k = WAVELEN(dl, T, 50, g)
+    def fileOutputRequestInit(self):
+        self.fileOutputRequestMain(defaultFilename = "toe_design")
 
-    steep, maxstp = ERRSTP(H, dl, L)
-    if steep >= maxstp:
-        print('Error: Input wave unstable (Max: #0.4f, [H / L]  =  #0.4f)' % (maxstp, steep))
-        return
+    def getCalcValues(self, caseInputList):
+        currIndex = 0
 
-    b1 = de * Kp
-    b2 = 2 * H
-    b3 = 0.4 * ds
-    b = [1, b1, b2, b3]
-    b = max(b)
+        if hasattr(self, "defaultValueH"):
+            H = self.defaultValueH
+        else:
+            H = caseInputList[currIndex]
+            currIndex = currIndex + 1
 
-    arg1 = (4 * math.pi * dl / L)
-    kappa = (arg1 / math.sinh(arg1)) * ((math.sin(2 * math.pi * b / L))**2)
-    arg2 = ((1 - kappa) / (kappa**(1 / 3))) * (dl / H)
-    Ns = 1.3 * arg2 + 1.8 * math.exp(-1.5 * (1 - kappa) * arg2)
+        if hasattr(self, "defaultValueT"):
+            T = self.defaultValueT
+        else:
+            T = caseInputList[currIndex]
+            currIndex = currIndex + 1
 
-    if Ns < 1.8:
-        Ns = 1.8
+        if hasattr(self, "defaultValue_ds"):
+            ds = self.defaultValue_ds
+        else:
+            ds = caseInputList[currIndex]
+            currIndex = currIndex + 1
 
-    w = (unitwt * (H**3)) / ((Ns**3) * ((specgrav - 1)**3))
+        if hasattr(self, "defaultValue_cotphi"):
+            cotphi = self.defaultValue_cotphi
+        else:
+            cotphi = caseInputList[currIndex]
+            currIndex = currIndex + 1
 
-    print('\n%s \t\t\t %-6.2f \t\n' % ('Width of toe apron' , b))
-    print('%s \t %-6.2f \t\n' % ('Weight of individual armor unit', w))
-    print('%s \t\t %-6.2f \t\n' % ('Water depth at top of tow', dl))
+        if hasattr(self, "defaultValueKp"):
+            Kp = self.defaultValueKp
+        else:
+            Kp = caseInputList[currIndex]
+            currIndex = currIndex + 1
 
-toe_design(0, 0, 0, 0, 0, 0, 0, 0)
+        if hasattr(self, "defaultValue_de"):
+            de = self.defaultValue_de
+        else:
+            de = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_ht"):
+            ht = self.defaultValue_ht
+        else:
+            ht = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_unitwt"):
+            unitwt = self.defaultValue_unitwt
+        else:
+            unitwt = caseInputList[currIndex]
+
+        return H, T, ds, cotphi, Kp, de, ht, unitwt
+    # end getCalcValues
+
+    def performCalculations(self, caseInputList, caseIndex = 0):
+        H, T, ds, cotphi, Kp, de, ht, unitwt =\
+            self.getCalcValues(caseInputList)
+
+        H20weight = self.g * self.rho
+
+        specgrav = unitwt / H20weight
+
+        dl = ds - ht
+        m = 1.0 / cotphi
+
+        if not (ds / (T**2) > 0.0037424):
+            print("Error: Limiting value detected...Hbs cannot be solved.")
+            return
+
+        Hbs = ERRWAVBRK2(T, m, ds)
+        if not (H < Hbs):
+            print("Error: Wave broken at structure (Hbs = %6.2f %s" %\
+                (Hbs, self.labelUnitDist))
+
+        L, k = WAVELEN(dl, T, 50, self.g)
+
+        steep, maxstp = ERRSTP(H, dl, L)
+        if not (steep < maxstp):
+            print("Error: Input wave unstable (Max: %0.4f, [H/L] = %0.4f" % (maxstp, steeps))
+
+        b1 = de*Kp
+        b2 = 2.0*H
+        b3 = 0.4*ds
+        b = [1.0, b1, b2, b3]
+        b = max(b)
+
+        arg1 = (4.0*math.pi*dl/L)
+        kappa = (arg1/math.sinh(arg1))*((math.sin(2.0*math.pi*b/L))**2)
+        arg2 = ((1.0 - kappa)/(kappa**(1.0/3.0)))*(dl/H)
+        Ns = 1.3*arg2 + 1.8*math.exp(-1.5*(1.0 - kappa)*arg2)
+
+        Ns = max(Ns, 1.8)
+
+        w = (unitwt*(H**3))/((Ns**3)*((specgrav - 1.0)**3))
+
+        print("\nWidth of toe apron\t\t%6.2f %s" %\
+            (b, self.labelUnitDist))
+        print("Weight of individual armor unit\t%6.2f %s" %\
+            (w, self.labelUnitWt))
+        print("Water depth at top of tow\t%6.2f %s" %\
+            (dl, self.labelUnitDist))
+
+        dataDict = {"H": H, "T": T, "ds": ds, "cotphi": cotphi,\
+            "Kp": Kp, "de": de, "ht": ht, "unitwt": unitwt,\
+            "b": b, "w": w, "dl": dl}
+        self.fileOutputWriteMain(dataDict, caseIndex)
+    # end performCalculations
+
+    def fileOutputWriteData(self, dataDict):
+        self.fileRef.write("Input\n")
+        self.fileRef.write("H\t\t\t\t%6.2f %s\n" %\
+            (dataDict["H"], self.labelUnitDist))
+        self.fileRef.write("T\t\t\t\t%6.2f s\n" % dataDict["T"])
+        self.fileRef.write("ds\t\t\t\t%6.2f %s\n" %\
+            (dataDict["ds"], self.labelUnitDist))
+        self.fileRef.write("cotphi\t\t\t\t%6.2f\n" % dataDict["cotphi"])
+        self.fileRef.write("Kp\t\t\t\t%6.2f\n" % dataDict["Kp"])
+        self.fileRef.write("de\t\t\t\t%6.2f %s\n" %\
+            (dataDict["de"], self.labelUnitDist))
+        self.fileRef.write("ht\t\t\t\t%6.2f %s\n" %\
+            (dataDict["ht"], self.labelUnitDist))
+        self.fileRef.write("unitwt\t\t\t\t%6.2f %s/%s^3\n" %\
+            (dataDict["unitwt"], self.labelUnitWt, self.labelUnitDist))
+
+        self.fileRef.write("\nWidth of toe apron\t\t%6.2f %s\n" %\
+            (dataDict["b"], self.labelUnitDist))
+        self.fileRef.write("Weight of individual armor unit\t%6.2f %s\n" %\
+            (dataDict["w"], self.labelUnitWt))
+        self.fileRef.write("Water depth at top of tow\t%6.2f %s\n" %\
+            (dataDict["dl"], self.labelUnitDist))
+    # end fileOutputWriteData
+
+
+driver = ToeDesign()
