3354b562642ed8be4753909c31216ecd82e8b666
Hendricks <hendricks.hicks@gmail.com>
2017-07-12 23:49:28 -0500

    some tide_generation

--- python/drivers/tide_generation.py
+++ python/drivers/tide_generation.py
@@ -20,7 +20,7 @@ from TIDELV import TIDELV
 # Provides a tide elevation record at a specific time and locale using
 # known amplitudes and epochs for individual harmonic constiuents
 
-# Updated by: Mary Anderson, USACE-CHL-Coastal Processes Branch
+# Updated by: Mary Anderson, USACE-CyearL-Coastal Processes Branch
 # Date Transferred: April 11, 2011
 # Date Verified: May 30, 2012
 
@@ -77,14 +77,6 @@ def tide_generation():
         'gauge longitude (deg)', -180, 180};
     [outputDataList] = USER_INPUT_FILE_INPUT_SINGLE(inputDataList);
 
-    year = outputDataList(1)
-    mon = outputDataList(2)
-    day = outputDataList(3)
-    hr = outputDataList(4)
-    tlhrs = outputDataList(5)
-    delt = outputDataList(6)
-    gauge0 = outputDataList(7)
-    glong = outputDataList(8)
 
     # MIGHT CHANGE IT AFTER CONFIRMATION
     m2ft = 3.28084
@@ -117,5 +109,224 @@ def tide_generation():
     plt.title('Tide Elevations [from constituents]')
     plt.plot(xtim,ytide)
     plt.show()
+
+class ToeDesign(BaseDriver):
+    def __init__(self, year = None, mon = None, day = None, hr = None,\
+        tlhrs = None, delt = None, gauge0 = None, glong = None):
+    year
+#   Hi: wave height (m)
+    mon
+#   T: wave period (sec)
+    day
+#   ds: water depth at structure (m)
+    hr
+#   cotphi: cotangent of nearshore slope
+    tlhrs
+#   Kp: passive earth pressure coefficient
+    delt
+#   de: sheet - pile penetration depth (m)
+    gauge0
+#   ht: height of toe protection layer above mudline
+    glong
+#   unitwt: unit weight of rock (N / m^3)
+        if year != None:
+            self.isSingleCase = True
+            self.defaultValue_year = year
+        if mon != None:
+            self.isSingleCase = True
+            self.defaultValue_mon = mon
+        if day != None:
+            self.isSingleCase = True
+            self.defaultValue_day = day
+        if cotphi != None:
+            self.isSingleCase = True
+            self.defaultValue_cotphi = cotphi
+        if Kp != None:
+            self.isSingleCase = True
+            self.defaultValueKp = Kp
+        if de != None:
+            self.isSingleCase = True
+            self.defaultValue_de = de
+        if ht != None:
+            self.isSingleCase = True
+            self.defaultValue_ht = ht
+        if unitwt != None:
+            self.isSingleCase = True
+            self.defaultValue_unitwt = unitwt
+
+        super(ToeDesign, self).__init__()
+    # end __init__
+
+    def userInput(self):
+        super(ToeDesign, self).userInput()
+
+        self.water, self.rho = USER_INPUT.SALT_FRESH_WATER(self.isMetric)
+    # end userInput
+
+    def defineInputDataList(self):
+        self.inputList = []
+
+        if not hasattr(self, "defaultValue_year"):
+            self.inputList.append(BaseField(\
+                "Hi: wave height (%s)" % (self.labelUnitDist), 0.1, 100.0))
+        if not hasattr(self, "defaultValueT"):
+            self.inputList.append(BaseField(\
+                "T: wave period (sec)", 1.0, 1000.0))
+        if not hasattr(self, "defaultValue_ds"):
+            self.inputList.append(BaseField(\
+                "ds: water depth at structure (%s)" % (self.labelUnitDist), 0.1, 200.0))
+        if not hasattr(self, "defaultValue_cotphi"):
+            self.inputList.append(BaseField(\
+                "cotphi: cotangent of nearshore slope", 5.0, 10000.0))
+        if not hasattr(self, "defaultValueKp"):
+            self.inputList.append(BaseField(\
+                "Kp: passive earth pressure coefficient", 0.0, 50.0))
+        if not hasattr(self, "defaultValue_de"):
+            self.inputList.append(BaseField(\
+                "de: sheet-pile penetration depth (%s)" % (self.labelUnitDist), 0.0, 200.0))
+        if not hasattr(self, "defaultValue_ht"):
+            self.inputList.append(BaseField(\
+                "ht: height of toe protection layer above mudline (%s)" % (self.labelUnitDist), 0.1, 200.0))
+        if not hasattr(self, "defaultValue_unitwt"):
+            self.inputList.append(BaseField(\
+                "unitwt: unit weight of rock (%s/%s^3)" % (self.labelUnitWt, self.labelUnitDist), 1.0, 99999.0))
+    # end defineInputDataList
+
+    def fileOutputRequestInit(self):
+        self.fileOutputRequestMain(defaultFilename = "toe_design")
+
+    def getCalcValues(self, caseInputList):
+        currIndex = 0
+
+        if hasattr(self, "defaultValue_year"):
+            year = self.defaultValue_year
+        else:
+            year = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValueT"):
+            T = self.defaultValueT
+        else:
+            T = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_ds"):
+            ds = self.defaultValue_ds
+        else:
+            ds = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_cotphi"):
+            cotphi = self.defaultValue_cotphi
+        else:
+            cotphi = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValueKp"):
+            Kp = self.defaultValueKp
+        else:
+            Kp = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_de"):
+            de = self.defaultValue_de
+        else:
+            de = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_ht"):
+            ht = self.defaultValue_ht
+        else:
+            ht = caseInputList[currIndex]
+            currIndex = currIndex + 1
+
+        if hasattr(self, "defaultValue_unitwt"):
+            unitwt = self.defaultValue_unitwt
+        else:
+            unitwt = caseInputList[currIndex]
+
+        return year, T, ds, cotphi, Kp, de, ht, unitwt
+    # end getCalcValues
+
+    def performCalculations(self, caseInputList, caseIndex = 0):
+        H, T, ds, cotphi, Kp, de, ht, unitwt =\
+            self.getCalcValues(caseInputList)
+
+        H20weight = self.g * self.rho
+
+        specgrav = unitwt / H20weight
+
+        dl = ds - ht
+        m = 1.0 / cotphi
+
+        if not (ds / (T**2) > 0.0037424):
+            print("Error: Limiting value detected...Hbs cannot be solved.")
+            return
+
+        Hbs = ERRWAVBRK2(T, m, ds)
+        if not (H < Hbs):
+            print("Error: Wave broken at structure (Hbs = %6.2f %s" %\
+                (Hbs, self.labelUnitDist))
+
+        L, k = WAVELEN(dl, T, 50, self.g)
+
+        steep, maxstp = ERRSTP(H, dl, L)
+        if not (steep < maxstp):
+            print("Error: Input wave unstable (Max: %0.4f, [H/L] = %0.4f" % (maxstp, steeps))
+
+        b1 = de*Kp
+        b2 = 2.0*H
+        b3 = 0.4*ds
+        b = [1.0, b1, b2, b3]
+        b = max(b)
+
+        arg1 = (4.0*math.pi*dl/L)
+        kappa = (arg1/math.sinh(arg1))*((math.sin(2.0*math.pi*b/L))**2)
+        arg2 = ((1.0 - kappa)/(kappa**(1.0/3.0)))*(dl/H)
+        Ns = 1.3*arg2 + 1.8*math.exp(-1.5*(1.0 - kappa)*arg2)
+
+        Ns = max(Ns, 1.8)
+
+        w = (unitwt*(H**3))/((Ns**3)*((specgrav - 1.0)**3))
+
+        print("\nWidth of toe apron\t\t%6.2f %s" %\
+            (b, self.labelUnitDist))
+        print("Weight of individual armor unit\t%6.2f %s" %\
+            (w, self.labelUnitWt))
+        print("Water depth at top of tow\t%6.2f %s" %\
+            (dl, self.labelUnitDist))
+
+        dataDict = {"H": H, "T": T, "ds": ds, "cotphi": cotphi,\
+            "Kp": Kp, "de": de, "ht": ht, "unitwt": unitwt,\
+            "b": b, "w": w, "dl": dl}
+        self.fileOutputWriteMain(dataDict, caseIndex)
+    # end performCalculations
+
+    def fileOutputWriteData(self, dataDict):
+        self.fileRef.write("Input\n")
+        self.fileRef.write("H\t\t\t\t%6.2f %s\n" %\
+            (dataDict["H"], self.labelUnitDist))
+        self.fileRef.write("T\t\t\t\t%6.2f s\n" % dataDict["T"])
+        self.fileRef.write("ds\t\t\t\t%6.2f %s\n" %\
+            (dataDict["ds"], self.labelUnitDist))
+        self.fileRef.write("cotphi\t\t\t\t%6.2f\n" % dataDict["cotphi"])
+        self.fileRef.write("Kp\t\t\t\t%6.2f\n" % dataDict["Kp"])
+        self.fileRef.write("de\t\t\t\t%6.2f %s\n" %\
+            (dataDict["de"], self.labelUnitDist))
+        self.fileRef.write("ht\t\t\t\t%6.2f %s\n" %\
+            (dataDict["ht"], self.labelUnitDist))
+        self.fileRef.write("unitwt\t\t\t\t%6.2f %s/%s^3\n" %\
+            (dataDict["unitwt"], self.labelUnitWt, self.labelUnitDist))
+
+        self.fileRef.write("\nWidth of toe apron\t\t%6.2f %s\n" %\
+            (dataDict["b"], self.labelUnitDist))
+        self.fileRef.write("Weight of individual armor unit\t%6.2f %s\n" %\
+            (dataDict["w"], self.labelUnitWt))
+        self.fileRef.write("Water depth at top of tow\t%6.2f %s\n" %\
+            (dataDict["dl"], self.labelUnitDist))
+    # end fileOutputWriteData
+
+
+driver = ToeDesign()
 
 tide_generation()
